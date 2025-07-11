863deb458bbbd1f53849cc2ad46afddb6c71fff1
Atharv Rawa <atharvrawal04@gmail.com>
2025-07-12 03:31:32 +0530

    v1

---
+++ Reset.md
@@ -0,0 +1,32 @@
+### Booting from partition on startup (Windows into a specific folder)
+- `sudo blkid` : lists all disks present on system with respective UUID & type
+- `sudo nano /etc/fstab`
+- Add the below line
+	- `UUID=xxxx-xxxx-xxxx-xxxx  /mount/point  file-system  mounting-opt  0  0`
+	- file-system : `ntfs-3g`, `ext4`, `exfat` etc..
+	- mounting-opt : `defaults`, `rw`, `realtime` etc.. {use defaults as mounting option}
+	- dump : 0 or 1 i.e. disable or enable backup dump utility {keep 0, no one cares}
+	- file system check : 0 or 1 i.e. disables or enables fsck {keep 0, no one cares} 
+	- Hence, final line :
+		- `UUID=EAAE69DBAE69A0B5    /mnt/Windows    ntfs-3g    defaults    0    0`
+- To and a symbolic link to the home dir
+	- `mkdir Windows`
+	- `ln -s /mnt/Windows/Users/Atharv /home/atharv/Windows` 
+	- `ln -s /mnt/Windows /home/atharv/Windows` 
+	```bash
+	atharvrawal@fedora:~/Windows$ tree
+	.
+	├── Atharv -> /mnt/windows/Users/Atharv
+	└── windows -> /mnt/windows
+	```
+
+### Slow Shutdown Time Solve
+1. 
+	- `sudo nvim /etc/systemd/system.conf`
+	- un-comment the line `DefaultTimeoutStopSec=90s`
+	- Then make the default timeout `10s`
+2. 
+	- `sudo nvim /usr/lib/systemd/user/gnome-session-restart-dbus.service` 
+	- Add `Slice=.slice` to the bottom of the file under `[Service]`
+
+### 
---
+++ install.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+echo "========================================"
+echo "Setup is Starting..."
+echo "========================================"
+
+
+# Function to check the exit status of the last command
+check_status(){
+	if [ $? -ne 0 ]; then
+		echo "Error: $1"
+		exit 1
+	fi
+}
+
+echo ""
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+check_status "Failed to set Dark Mode"
+echo "Dark Mode Successful"
+echo ""
+
+echo "Setting Keybings..."
+echo "Switch Windows            : Alt    +   Tab"
+echo "Switch Application        : Super  +   Tab"
+echo "Switch to Workspace Left  : Alt    +   H"
+echo "Switch to Workspace Right : Alt    +   L"
+echo "Launch Nautilus           : Super  +   E"
+echo "Move to Workspace Left    : Alt    +   Shift  +  H"
+echo "Move to Workspace Right   : Alt    +   Shift  +  L"
+echo "Show Desktop              : Super  +   D"
+echo "Toggle Maximization State : Super  +   Up"
+echo ""
+gsettings set org.gnome.desktop.wm.keybindings switch-windows             "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications        "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left   "['<Alt>H']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right  "['<Alt>L']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys home           "['<Super>E']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left     "['<Alt><Shift>H']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right    "['<Alt><Shift>L']"
+gsettings set org.gnome.desktop.wm.keybindings show-desktop               "['<Super>D']"
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized           "['<Super>Up']"
+gsettings set org.gnome.desktop.wm.keybindings close                      "['<Super>C']"
+
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile           "flat"
+gsettings set org.gnome.shell favorite-apps                               "[]"
+
+
+echo "Updating & Upgrading Arch..."
+sudo pacman -Syy --noconfirm >/dev/null
+check_status "Failed to update the package database."
+sudo pacman -Syu --noconfirm >/dev/null
+check_status "Failed to upgrade the package database."
+echo "Successfully Updated & Upgraded"
+echo ""
+
+echo "Installing pacman packages..."
+pacman_packages=(
+	curl
+	git
+	nvim
+	tree
+	nmap
+	wget
+	base-devel
+	cmake
+	obs-studio
+	flatpak
+	net-tools
+)
+
+i=0
+total=${#pacman_packages[@]}
+
+while [ $i -lt $total ]; do
+  pkg=${pacman_packages[$i]}
+  sudo pacman -S --needed --noconfirm "$pkg" >/dev/null 2>&1
+  check_status "Failed to install $pkg"
+  echo "✅ $pkg installed successfully"
+  ((i++))
+done
+echo "All pacman packages installed successfully"
+echo ""
+
+
+echo "Installing Yay..."
+git clone https://aur.archlinux.org/yay.git >/dev/null 2>&1
+check_status "Failed to clone yay repository."
+cd yay || { echo "Failed to change directory to yay."; exit 1; }
+makepkg -si --noconfirm >/dev/null 2>&1
+check_status "Failed to build and install yay."
+yay --version
+cd ..
+sudo rm -rf yay >/dev/null
+check_status "Failed to remove the yay package"
+echo "Above Version is yay version and is Successfully Installed"
+echo ""
+
+
+yay_packages=(
+	"elecwhat-bin"
+)
+
+
+j=0
+total=${#yay_packages[@]}
+
+while [ $j -lt $total ]; do
+  pkg=${yay_packages[$j]}
+  yay -S --needed --noconfirm "$pkg" >/dev/null 2>&1
+  check_status "Failed to install $pkg"
+  echo "✅ $pkg installed successfully"
+  ((j++))
+done
+echo "All yay packages installed succesfully"
+echo ""
+
+
+
+echo "Installing flatpacks..."
+flatpacks=(
+  "md.obsidian.Obsidian"
+  "com.brave.Browser"
+  "com.discordapp.Discord"
+  "com.spotify.Client"
+  "com.obsproject.Studio"
+  "org.kde.kate"
+  "com.visualstudio.code"
+  "org.videolan.VLC"
+  "com.rafaelmardojai.Blanket"
+)
+
+k=0 
+total=${#flatpacks[@]}
+while [ $k -lt $total ]; do
+  pkg=${flatpacks[$k]}
+  flatpak install -y --noninteractive flathub "$pkg" >/dev/null 2>&1
+  check_status "Failed to install $pkg"
+  echo "✅ $pkg installed successfully"
+  ((k++))
+done
+echo "All flatpak packages installed successfully!"
